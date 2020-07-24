a966256b217f2c881531cf802747ba23e32858ff
unknown <Stefan Vranjes@STEFANFRANJES.planetsoft.ba>
2020-07-24 14:59:36 +0200

    Zavrsen navbar i login

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,16 +1,67 @@
 import React from "react";
 import Bcg from "../img/bcg-traffic.jpg";
-import { Container, Row, Col } from "react-bootstrap";
+import Googleimg from "../img/icons-google.png";
+import Link from "./link.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 
 const Home = (props) => {
   return (
-    <Container fluid>
-      <Row>
-        <Col>
-          <img src={Bcg} className="bcg-image" alt="Bcg" />
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <div className="bcg-image">
+        <img src={Bcg} className="bcg-image" alt="Bcg" />
+      </div>
+
+      <div className="sv-login-modal">
+        <div className="row">
+          <div className="col-12 text-center mb-3">
+            <h4>Login</h4>
+          </div>
+        </div>
+        <form>
+          <div className="row">
+            <div className="col-6 col-md-9 col-lg-12 col-xl-12 align-self-center">
+              <input className="form-control" placeholder="Username"></input>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-6 col-md-9 col-lg-12 col-xl-12 align-self-center mt-3">
+              <input className="form-control" placeholder="Password"></input>
+            </div>
+          </div>
+
+          <div className="form-check mt-2">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+            />
+            <label className="form-check-label" for="exampleCheck1">
+              Remember me
+            </label>
+          </div>
+
+          <div className="row">
+            <div className="col-6 col-md-9 col-lg-12 col-xl-12 align-self-center mt-3">
+              <button className="form-control">Sing in</button>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-4 col-lg-4 align-self-center mt-3 text-center">
+              <Link href="#">
+                <FontAwesomeIcon icon={faFacebook} />
+              </Link>
+            </div>
+            <div className="col-md-2 col-lg-4 align-self-center mt-3 text-center">
+              <Link href="#" className="google-icon">
+                <img src={Googleimg} />
+              </Link>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
   );
 };
 
--- frontend/src/components/offer.js
+++ frontend/src/components/offer.js
@@ -1,11 +1,23 @@
 import React from "react";
-
 function Offer(props) {
   return (
-    <div>
-      Test111
-      <button>test</button>
-    </div>
+    <>
+      <div className="container mt-4">
+        <div className="row m-0 m-auto">
+          <div className="col-12 col-md-7 col-lg-7 mr-2 mb-2 sv-test">
+            seacrh
+          </div>
+          <div className="col-12 col-md-2 col-lg-2 mr-2 mb-2 sv-test">1</div>
+          <div className="col-12 col-md-2 col-lg-2 mb-2 sv-test">1</div>
+        </div>
+      </div>
+
+      <div className="container-fluid mt-4">
+        <div className="row m-0 m-auto">
+          <div class="col-12 col-md-11 col-lg-11 m-0 m-auto sv-test"></div>
+        </div>
+      </div>
+    </>
   );
 }
 
