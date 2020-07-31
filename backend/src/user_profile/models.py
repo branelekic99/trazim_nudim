b68365a3e7d2822db057ad309b8d423f3b8da8a7
Branisalv Lekic <blekic@planetsoft.ba>
2020-07-31 11:47:49 +0200

    redux login and user handle functions

--- backend/src/user_profile/models.py
+++ backend/src/user_profile/models.py
@@ -14,7 +14,6 @@ class Profile(models.Model):
         return self.user.username
 
 def create_profile(sender,**kwargs):
-    print(kwargs)
     if kwargs['created']:
         Profile.objects.create(user=kwargs['instance'])
 
