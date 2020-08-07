3c94e671c385c9014a70757ed7af64fc8ebde420
unknown <msormaz@MILANSORMAZ.planetsoft.ba>
2020-08-07 15:36:16 +0200

    Chanege attributes in models

--- backend/src/user_profile/models.py
+++ backend/src/user_profile/models.py
@@ -2,16 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
+
 class Profile(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     profile_pic = models.ImageField(blank=True, null=True,upload_to='profile_pics')
-    adresa = models.CharField(max_length=100,blank=True)
-    datum_reg = models.DateField(auto_now_add=True)
-    broj_tell = models.CharField(max_length=30,blank=True)
-    goditste = models.IntegerField(blank=True,null=True)
+    adddress = models.CharField(max_length=100,blank=True)
+    reg_date = models.DateField(auto_now_add=True)
+    tel_number = models.CharField(max_length=30,blank=True)
+    age = models.IntegerField(blank=True,null=True)
 
-    def __str__(self):
-        return self.user.username
+def Meta:
+    db_table = "Profile"
+
+def __str__(self):
+    return self.user.username
 
 def create_profile(sender,**kwargs):
     if kwargs['created']:
