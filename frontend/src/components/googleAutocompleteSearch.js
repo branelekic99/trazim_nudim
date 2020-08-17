7d2959f9689462655f6b71c747e2606aedb9b131
Stefan Vranjes <STEFANVRANJES@planetsoft.ba>
2020-08-17 09:31:34 +0200

    Azuriran kod

--- frontend/src/components/googleAutocompleteSearch.js
+++ frontend/src/components/googleAutocompleteSearch.js
@@ -1,47 +1,58 @@
-import React ,{useState} from 'react';
-import PlacesAutocomplete,{geocodeByAddress,getLatLng} from 'react-places-autocomplete';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import TextField from '@material-ui/core/TextField';
+import React, { useState } from "react";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from "react-places-autocomplete";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import TextField from "@material-ui/core/TextField";
 
+function AutoSerach(props) {
+  const [address, setAddress] = useState("");
 
-function AutoSerach(props){
+  const handleSelect = async (value) => {
+    const results = await geocodeByAddress(value);
+    const latLng = await getLatLng(results[0]);
+    setAddress(value);
+    props.onLocationChange(latLng);
+  };
 
-    const [address,setAddress] = useState("");
-
-    const handleSelect = async value => {
-      const results = await geocodeByAddress(value);
-      const latLng = await getLatLng(results[0]);
-      setAddress(value);
-      props.onLocationChange(latLng);
-    };
-  
-    return(
-        <PlacesAutocomplete
-          value={address}
-          onChange={setAddress}
-          onSelect={handleSelect}
-        >
-        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+  return (
+    <PlacesAutocomplete
+      value={address}
+      onChange={setAddress}
+      onSelect={handleSelect}
+    >
+      {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+        <div>
+          <TextField
+            inputRef={props.register}
+            label={props.label}
+            {...getInputProps()}
+            fullWidth
+            name={props.label}
+          />
           <div>
-              <TextField inputRef={props.register} label={props.label} {...getInputProps()} fullWidth name={props.label} />
-            <div>
-              {/* {loading ? <div>...loading</div> : null} */}
+            {/* {loading ? <div>...loading</div> : null} */}
             <List>
-              {suggestions.map((suggestion,id) => {
+              {suggestions.map((suggestion, id) => {
                 return (
-                  <ListItem {...getSuggestionItemProps(suggestion)} key={id} button>
+                  <ListItem
+                    {...getSuggestionItemProps(suggestion)}
+                    key={id}
+                    button
+                  >
                     <ListItemText primary={suggestion.description} />
                   </ListItem>
                 );
               })}
-              </List>
-            </div>
+            </List>
           </div>
-        )} 
-      </PlacesAutocomplete>
-    )
-};
+        </div>
+      )}
+    </PlacesAutocomplete>
+  );
+}
 
 export default AutoSerach;
